a5c523809ca82dcc226ee63bd27d302bf5cb25fb
twhsu-stanley <twhsu@umich.edu>
2026-03-05 19:41:59 -0600

    na=1; CCM only depends on x([3,4]) and a(2); deg 4

--- examples/planarquad/uncertain_dynamics/metric/main_uncertain_planarquad_metric.m
+++ examples/planarquad/uncertain_dynamics/metric/main_uncertain_planarquad_metric.m
@@ -70,8 +70,8 @@ if na == 1
     W_states = [x(Wstates_index); a]; % extend W_states to incorporate a
     v_W = monolist(W_states, 4);
 elseif na == 2
-    W_states = [x(Wstates_index); a]; % extend W_states to incorporate a
-    v_W = monolist(W_states, 3); % monomials of W_states up to some degree
+    W_states = [x(Wstates_index); a(2)]; % extend W_states to incorporate a
+    v_W = monolist(W_states, 4); % monomials of W_states up to some degree
     %v_W = kron(monolist(x(Wstates_index), 3), monolist(a, 1));
 elseif na == 4
     W_states = [x(Wstates_index); a(1:3)]; % extend W_states to incorporate a
